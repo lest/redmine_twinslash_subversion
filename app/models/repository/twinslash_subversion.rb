e2f252ba7e8402d987888f2e2e9a71f94cbbb9b1
lest <just.lest@gmail.com>
2010-05-05 10:28:13 +0300

    don't manage svnauthz config file and create repo with url based on project identifier

--- app/models/repository/twinslash_subversion.rb
+++ app/models/repository/twinslash_subversion.rb
@@ -1,22 +1,16 @@
 class Repository::TwinslashSubversion < Repository::Subversion
-  validates_presence_of :name
-  
   def self.scm_name
     'Twinslash Subversion'
   end
-  
-  def name=(arg)
+
+  def project=(project)
+    super
     base_url = Setting.plugin_redmine_twinslash_subversion['base_url']
-    write_attribute(:url, arg ? base_url + arg : nil)
+    write_attribute(:url, base_url + project.identifier)
   end
-  
-  def name
-    base_url = Setting.plugin_redmine_twinslash_subversion['base_url']
-    self.url ? self.url.sub(Regexp.new("^#{Regexp.quote(base_url)}"), '') : nil
-  end
-  
+
   def after_save
     create_command = Setting.plugin_redmine_twinslash_subversion['create_command']
-    system("#{create_command} #{self.name}")
+    system("#{create_command} #{project.identifier}")
   end
 end
--- init.rb
+++ init.rb
@@ -4,16 +4,12 @@ Redmine::Plugin.register :redmine_twinslash_subversion do
   name 'Twinslash Subversion plugin'
   author 'Just Lest'
   description ''
-  version '0.0.1'
+  version '0.1.0'
 
   settings :default => {
     'base_url' => '',
-    'config_location' => '',
     'create_command' => ''
   }, :partial => 'settings/twinslash_subversion_settings'
 end
 
-REDMINE_SUPPORTED_SCM.push('TwinslashSubversion')
-
-ActiveRecord::Base.observers << :repository_twinslash_subversion_observer
-ActiveRecord::Base.observers << :member_twinslash_subversion_observer
+Redmine::Scm::Base.add 'TwinslashSubversion'
